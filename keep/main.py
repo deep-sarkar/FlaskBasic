e466ea05a8d89a934472c00a7d1d8fcd5fa00a72
deep-sarkar <deepsarkar96@gmail.com>
2022-03-16 10:42:08 +0530

    Added project structure

---
+++ keep/main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Api
+
+from db.utils import connect_db
+from routes import all_routes
+app = Flask(__name__)
+api = Api(app)
+
+
+def config_app_routes():
+    for route in all_routes:
+        end_point = route[0]
+        handler = route[1]
+        api.add_resource(handler, end_point)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return '<h1>Hello World</h1>'
+
+
+# Driver code
+config_app_routes()
+connect_db()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ keep/routes.py
@@ -0,0 +1,4 @@
+import user
+import fundoo
+
+all_routes = user.api_routes + fundoo.api_routes
